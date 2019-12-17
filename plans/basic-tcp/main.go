7c74500a3070487af45984d970fadb2c37ceb1bd
Jim Pick <jim@protocol.ai>
2019-12-16 23:17:09 -0800

    Fix problem due to early exiting
    Also `go fmt`

--- plans/basic-tcp/main.go
+++ plans/basic-tcp/main.go
@@ -15,10 +15,10 @@ import (
 )
 
 var (
-        MetricBytesSent    = &runtime.MetricDefinition{Name: "sent_bytes", Unit: "bytes", ImprovementDir: -1}
-        MetricBytesReceived = &runtime.MetricDefinition{Name: "received_bytes", Unit: "bytes", ImprovementDir: -1}
-        MetricTimeToSend     = &runtime.MetricDefinition{Name: "time_to_send", Unit: "ms", ImprovementDir: -1}
-        MetricTimeToReceive = &runtime.MetricDefinition{Name: "time_to_receive", Unit: "ms", ImprovementDir: -1}
+	MetricBytesSent     = &runtime.MetricDefinition{Name: "sent_bytes", Unit: "bytes", ImprovementDir: -1}
+	MetricBytesReceived = &runtime.MetricDefinition{Name: "received_bytes", Unit: "bytes", ImprovementDir: -1}
+	MetricTimeToSend    = &runtime.MetricDefinition{Name: "time_to_send", Unit: "ms", ImprovementDir: -1}
+	MetricTimeToReceive = &runtime.MetricDefinition{Name: "time_to_receive", Unit: "ms", ImprovementDir: -1}
 )
 
 var peerIPSubtree = &sdksync.Subtree{
@@ -45,7 +45,6 @@ func main() {
 		runenv.Abort(err)
 		return
 	}
-	// fmt.Fprintln(os.Stderr, "Addrs:", ifaddrs)
 
 	_, localnet, _ := net.ParseCIDR("8.0.0.0/8")
 
@@ -58,13 +57,11 @@ func main() {
 		case *net.IPAddr:
 			ip = v.IP
 		}
-		// fmt.Fprintln(os.Stderr, "IP:", ip)
 		if localnet.Contains(ip) {
 			peerIP = ip
 			break
 		}
 	}
-	//fmt.Fprintln(os.Stderr, "Matched IP:", peerIP)
 	if peerIP == nil {
 		runenv.Abort("No IP match")
 		return
@@ -76,22 +73,22 @@ func main() {
 	if withShaping {
 		runenv.Message("Waiting for network to be initialized")
 		if err := sdksync.WaitNetworkInitialized(ctx, runenv, watcher); err != nil {
-		    runenv.Abort(err)
-		    return
+			runenv.Abort(err)
+			return
 		}
 		runenv.Message("Network initialized")
 
 		config := sdksync.NetworkConfig{
-		    // Control the "default" network. At the moment, this is the only network.
-		    Network: "default",
+			// Control the "default" network. At the moment, this is the only network.
+			Network: "default",
 
-		    // Enable this network. Setting this to false will disconnect this test 
-		    // instance from this network. You probably don't want to do that.
-		    Enable:  true,
+			// Enable this network. Setting this to false will disconnect this test
+			// instance from this network. You probably don't want to do that.
+			Enable: true,
 		}
 		config.Default = sdksync.LinkShape{
-		    // Latency:   100 * time.Millisecond,
-		    Bandwidth: 1 << 20, // 1Mib
+			// Latency:   100 * time.Millisecond,
+			Bandwidth: 1 << 20, // 1Mib
 		}
 		config.State = "network-configured"
 
@@ -103,14 +100,14 @@ func main() {
 
 		_, err = writer.Write(sdksync.NetworkSubtree(hostname), &config)
 		if err != nil {
-		    runenv.Abort(err)
-		    return
+			runenv.Abort(err)
+			return
 		}
 
 		err = <-watcher.Barrier(ctx, config.State, int64(runenv.TestInstanceCount))
 		if err != nil {
-		    runenv.Abort(err)
-		    return
+			runenv.Abort(err)
+			return
 		}
 		runenv.Message("Network configured")
 	}
@@ -124,6 +121,7 @@ func main() {
 
 	// States
 	ready := sdksync.State("ready")
+	received := sdksync.State("received")
 
 	switch {
 	case seq == 1: // receiver
@@ -147,11 +145,11 @@ func main() {
 			runenv.Abort(err)
 			return
 		}
-		// fmt.Fprintln(os.Stderr, "State: ready")
+		// runenv.Message("State: ready")
 
 		var wg sync.WaitGroup
 		wg.Add(runenv.TestInstanceCount - 1)
-		// fmt.Fprintln(os.Stderr, "Waiting for connections:", runenv.TestInstanceCount-1)
+		// runenv.Message(fmt.Sprintf("Waiting for connections: %v", runenv.TestInstanceCount-1))
 
 		go func() {
 			for {
@@ -159,12 +157,14 @@ func main() {
 				if err != nil {
 					select {
 					case <-quit:
+						// runenv.Message("Accepted, but quitting")
 						return
 					default:
 						runenv.Abort(err)
 						return
 					}
 				}
+				// runenv.Message("Accepted")
 				go func(c net.Conn) {
 					defer c.Close()
 					bytesRead := 0
@@ -175,7 +175,12 @@ func main() {
 						bytesRead += n
 						// runenv.Message(fmt.Sprintf("Received %v", n))
 						if err == io.EOF {
+							// runenv.Message("EOF")
 							break
+						} else if err != nil {
+							fmt.Fprintln(os.Stderr, "Error", err)
+							wg.Done()
+							return
 						}
 					}
 					runenv.EmitMetric(MetricBytesReceived, float64(bytesRead))
@@ -186,6 +191,14 @@ func main() {
 		}()
 
 		wg.Wait()
+
+		// Signal we've received all the data
+		_, err = writer.SignalEntry(received)
+		if err != nil {
+			runenv.Abort(err)
+			return
+		}
+		// runenv.Message("State: received")
 
 	case seq == 2: // sender
 		runenv.Message(fmt.Sprintf("Sender: %v", peerIP))
@@ -208,15 +221,15 @@ func main() {
 		}
 
 		// Wait until ready state is signalled.
-		// fmt.Fprintln(os.Stderr, "Waiting for ready")
+		// runenv.Message("Waiting for ready")
 		readyCh := watcher.Barrier(ctx, ready, 1)
 		if err := <-readyCh; err != nil {
 			panic(err)
 		}
-		// fmt.Fprintln(os.Stderr, "State: ready")
+		// runenv.Message("State: ready")
 
 		for _, peerIPToDial := range peerIPsToDial {
-			// fmt.Fprintln(os.Stderr, "Dialing", peerIPToDial)
+			// runenv.Message(fmt.Sprintf("Dialing %v", peerIPToDial))
 			conn, err := net.Dial("tcp", peerIPToDial.String()+":2000")
 			if err != nil {
 				fmt.Fprintln(os.Stderr, "Error", err)
@@ -230,7 +243,7 @@ func main() {
 			tstarted := time.Now()
 			for i := 0; i < 10; i++ {
 				n, err := conn.Write(buf)
-				// fmt.Fprintln(os.Stderr, "Sent", n)
+				// runenv.Message(fmt.Sprintf("Sent %v", n))
 				bytesWritten += n
 				if err != nil {
 					fmt.Fprintln(os.Stderr, "Error", err)
@@ -241,6 +254,14 @@ func main() {
 			runenv.EmitMetric(MetricTimeToSend, float64(time.Now().Sub(tstarted)/time.Millisecond))
 			conn.Close()
 		}
+
+		// Wait until all data is received before shutting down
+		// runenv.Message("Waiting for received state")
+		receivedCh := watcher.Barrier(ctx, received, 1)
+		if err := <-receivedCh; err != nil {
+			panic(err)
+		}
+		// runenv.Message("State: received")
 
 	default:
 		runenv.Abort(fmt.Errorf("Unexpected seq: %v", seq))
